8b86220ea4c33f0bd5c4c2fd0e91a01b2f54dfd5
Nguyen Vu <nguyenanhvu2172000@gmail.com>
2023-04-18 00:08:19 +0700

    auth using google account

--- FE/src/models/api.ts
+++ FE/src/models/api.ts
@@ -20,3 +20,36 @@ export interface MovieListData {
   updated: string;
   name: string;
 }
+
+export interface ProvType {
+  name: string;
+  state: string;
+  codeVerifier: string;
+  codeChallenge: string;
+  codeChallengeMethod: string;
+  authUrl: string;
+}
+
+export interface UserType {
+  id: string;
+  created: string;
+  updated: string;
+  email: string;
+  verified: boolean;
+  lastResetSentAt: string;
+  lastVerificationSentAt: string;
+  profile: Profile;
+}
+
+export interface Profile {
+  id: string;
+  created: string;
+  updated: string;
+  "@collectionId": string;
+  "@collectionName": string;
+  avatar: string;
+  avatarUrl: string;
+  name: string;
+  userId: string;
+  "@expand": {};
+}
--- FE/src/utils/common.ts
+++ FE/src/utils/common.ts
@@ -14,8 +14,9 @@ export interface MenuSidebar {
 
 export const MENU_SIDEBAR = [
   { id: 1, name: "Home", path: "/" },
+  { id: 4, name: "Category", path: "/category" },
   { id: 2, name: "Movie", path: "/movie" },
-  { id: 3, name: "Vote", path: "/vote" },
+  { id: 3, name: "User", path: "/user" },
   { id: 4, name: "Company", path: "/company" },
   { id: 5, name: "Genre", path: "/genre" },
 ];
